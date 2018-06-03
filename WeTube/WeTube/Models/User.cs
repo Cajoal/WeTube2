f3b999e6835b2d7c7e7260f2ae0e22b14c65d3cc
Caleb Albertson <calbertson17@gmail.com>
2018-06-03 11:03:10 -0500

    Commiting

---
+++ WeTube/WeTube/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeTube.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string AvatarName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Password { get; set; }
+    }
+}
